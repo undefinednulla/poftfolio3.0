0fe0d8385cb5d89cf775f5521e0e9af870827d43
undefinednulla <buihahaidanggg@gmail.com>
2022-04-10 15:56:21 +0700

    web tinh

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,13 @@
-console.log("Hello Word")
+// let innerCursor = document.getElementsByClassName('.inner-cursor')
+// let outerCursor = document.getElementsByClassName('.outer-cursor')
+
+// document.addEventListener("mousemove", moveCursor);
+
+// function moveCursor(e) {
+//     let a = e.clientX
+//     let b = e.clientY
+//     innerCursor.style.left = `${a}px`
+//     outerCursor.style.left = `${a}px`
+//     innerCursor.style.top = `${b}px`
+//     outerCursor.style.top = `${b}px`
+// }
